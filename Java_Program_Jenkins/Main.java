db010f2ec2772c590562c8cf7aed8f81f523ebdf
Sushmita-Hubli <hublisushmita7@gmail.com>
2025-09-25 11:11:23 -0400

    initial commit

--- Java_Program_Jenkins/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class Main{
-    public static void main(String[] args) {
-        System.out.println("This is java program");
-        System.out.println("output from java program");
-        System.out.println(new Date());
-    }
-}
---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String args[])
+    {
+        System.out.println("Hello from Java Program");
+        System.out.println(new Date());
+
+    }
+}
